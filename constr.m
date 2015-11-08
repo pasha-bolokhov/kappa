05861ff9d2a9fe263dcec60cab4e48f3168cad4f
Pasha Bolokhov <pasha.bolokhov@gmail.com>
2015-11-08 10:17:42 -0800

    Initial revision

---
+++ constr.m
@@ -0,0 +1,30 @@
+
+e = 0.3
+
+mN = 0.938
+
+mrho = 0.776
+
+Lambda = mN
+
+  muN = e / (2.0 mN)
+
+mup = 2.79 muN
+
+mun = -1.9 muN
+
+  C1rho = ( mup - mun ) / e
+ 
+  C1  = mun / e
+
+cosW = 0.88
+
+  hypercharge = 1.0 / (Sqrt[2.0] 0.88)
+
+
+
+  Integ = (3 / (64 Pi^2)) Log [ Lambda / mrho ] C1 C1rho e^2 mrho^4 hypercharge
+
+  2 Integ
+
+
---
+++ constr_old.m
@@ -0,0 +1,14 @@
+
+e = 0.3
+
+mN = 0.938
+
+mrho = 0.776
+
+Lambda = 80.0
+
+cosW = 0.88
+
+
+  (3. / (128.0 Pi^2)) Log [ Lambda / mrho ] (e^2 / mN^2) mrho^4  / cosW
+
